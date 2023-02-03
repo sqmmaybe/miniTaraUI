158a993172f5a132b3a77296da5724b4327761ad
yanghanqing <yanghanqing@able-elec.com>
2023-02-03 16:54:22 +0800

    更改配置目录

--- packages/examples/vite.config.ts
+++ packages/examples/vite.config.ts
@@ -2,5 +2,21 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [vue()]
+    plugins: [vue()],
+    // build: {
+    //     terserOptions: {
+    //         compress: {
+    //             //生产环境时移除console
+    //             drop_console: true,
+    //             drop_debugger: true,
+    //         },
+    //     },
+    //     rollupOptions: {
+    //         output: {
+    //             chunkFileNames: 'js/[name]-[hash].js',
+    //             entryFileNames: 'js/[name]-[hash].js',
+    //             assetFileNames: '[ext]/[name]-[hash].[ext]',
+    //         },
+    //     },
+    // }
 });
